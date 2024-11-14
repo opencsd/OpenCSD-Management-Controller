b3bdc58110bb4db44c649330d9d29f83696c0516
juhyunkil <wngus0331@gmail.com>
2024-11-14 11:27:55 +0900

    delete opencsd metric collector

--- OpenCSD-Metric-Collector/backup/1.import.sh
+++
@@ -1,7 +0,0 @@
-#grpc, protoc 라이브러리
-go get -u google.golang.org/grpc
-go get -u github.com/golang/protobuf/protoc-gen-go
-
-#influxdb client 라이브러리 
-# go get github.com/influxdata/influxdb-client-go/v2
-go get github.com/influxdata/influxdb/client/v2
